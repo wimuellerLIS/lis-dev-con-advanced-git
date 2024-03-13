36b0134ac8dc936b812b80053b0f761aaf742986
Marvin Mees <marvin.mees@lis.eu>
2024-03-13 14:20:22 +0100

    Formatting

--- src/DemoProject.Console/Program.cs
+++ src/DemoProject.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Text.RegularExpressions;
 
 namespace DemoProject;
@@ -20,7 +21,7 @@ public class Program
       int Month = Convert.ToInt32(Match.Groups[0].Value.Split('-')[1]);
       int Day = Convert.ToInt32(Match.Groups[0].Value.Split('-')[2]);
 
-      Console.WriteLine($"The {Input} was a {CalculateDayOfWeek(Year, Month, Day)}");
+      Console.WriteLine($"The {Input} was a {CalculateDayOfWeek(Year, Month, Day)}") ;
     }
     else
       Console.WriteLine("WRONG FORMAT");
